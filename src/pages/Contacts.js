53bd0842293fc8af852cdf059e915adfaba85e55
Anton Holub <golubanton753@gmail.com>
2022-06-08 13:19:08 +0300

    delete page is about combo and apply contacts

---
+++ src/pages/Contacts.js
@@ -0,0 +1,13 @@
+import { Container } from "../components/Container";
+
+const Contacts = () => {
+    return ( 
+        <div className="Contacts">
+            <Container>
+                
+            </Container>
+        </div>
+     );
+}
+ 
+export default Contacts;
