1cab4c31ed46a23cd6074cc09f9eb0d77acc91f3
Anton Holub <golubanton753@gmail.com>
2022-06-22 15:52:56 +0300

    fix fetching data

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <HeaderContainer>
             <Container>
                 <HeaderWrapper>
-                    <Home to="/where-pizza">
+                    <Home to="/">
                         <Logo src="https://golub753.github.io/where-pizza/images/icons/logo.svg" alt="logo"/>
                         <Text>
                             Where Pizza
